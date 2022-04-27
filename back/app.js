8e897ac169fc5974368302873b98ad1bb979d59c
Thomas Bnt <thomasbnt@protonmail.com>
2022-04-27 16:14:02 +0200

    .idea stuffs

--- back/app.js
+++ back/app.js
@@ -6,10 +6,10 @@ const productRoutes = require('./routes/product');
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
