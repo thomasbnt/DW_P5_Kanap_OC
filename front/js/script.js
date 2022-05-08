58839da995a173eb3f35ca298b98ae95e9fce2c0
Thomas Bnt <thomasbnt@protonmail.com>
2022-05-08 15:38:46 +0200

    cleanup

--- front/js/script.js
+++ front/js/script.js
@@ -1,25 +1,23 @@
 const base_url = 'http://localhost:3000/api/';
 const product_url = base_url + 'products';
 
-const product_list = document.querySelector('#items')
+const product_list = document.querySelector('#items');
 
 fetch(product_url)
-    .then(response => response.json())
-    .then(data => {
-        console.table(data);
-        for (let d of data) {
-            let product = document.createElement('a');
-            product.href = `./product.html?id=${d._id}`
-            product.innerHTML = `
+  .then((response) => response.json())
+  .then((data) => {
+    console.table(data);
+    for (let d of data) {
+      let product = document.createElement('a');
+      product.href = `./product.html?id=${d._id}`;
+      product.innerHTML = `
             <article>
-              <img src="${d.imageUrl}" alt="${d.altTxt}">
-              <h3 class="productName">${d.name}</h3>
-              <p class="productDescription">${d.description}</p>
+              <img src='${d.imageUrl}' alt='${d.altTxt}'>
+              <h3 class='productName'>${d.name}</h3>
+              <p class='productDescription'>${d.description}</p>
             </article>
             `;
-            product_list.appendChild(product);
-            console.log(d);
-        }
-
-    });
-
+      product_list.appendChild(product);
+      console.log(d);
+    }
+  });
