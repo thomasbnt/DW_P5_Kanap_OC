5ee895c54f899f2ab819ae147a09d138954a3e22
Thomas Bnt <thomasbnt@protonmail.com>
2022-05-08 15:38:41 +0200

    cleanup + WIP LocalStorage

--- front/js/product.js
+++ front/js/product.js
@@ -9,79 +9,84 @@ const cart_list = document.querySelector('.item');
  */
 
 fetch(product_url).then(response => response.json())
-    .then(data => {
-        let url = new URL(window.location.href);
-        let id = url.searchParams.get('id');
-        console.log(`ID: ${id}`);
+  .then(data => {
+    let url = new URL(window.location.href);
+    let id = url.searchParams.get('id');
+    console.log(`ID: ${id}`);
 
-        let h = data.find(product => product._id === id);
-        console.table(h);
+    let h = data.find(product => product._id === id);
+    //console.table(h);
 
-        // On vérifie que l'id existe
-        h === undefined ? alert('Le produit n\'existe pas ou la page a été supprimée.') : null;
     // On vérifie que l'id existe
     if (h === undefined) {
       const ErrMessageInContent = document.querySelector('.item__content');
       ErrMessageInContent.innerHTML = '<h1>Le produit n\'existe pas ou la page a été supprimée.</h1>';
     }
 
-        // Récupère les ID pour par la suite les afficher à sa bonne place
-        const name_item = document.querySelector('#title');
-        const price_item = document.querySelector('#price');
-        const description_item = document.querySelector('#description');
+    // Récupère les ID pour par la suite les afficher à sa bonne place
+    const name_item = document.querySelector('#title');
+    const price_item = document.querySelector('#price');
+    const description_item = document.querySelector('#description');
 
-        // Afficher le nom, le prix et la description du produit
-        name_item.textContent = h.name;
-        price_item.textContent = h.price;
-        description_item.textContent = h.description;
+    // Afficher le nom, le prix et la description du produit
+    name_item.textContent = h.name;
+    price_item.textContent = h.price;
+    description_item.textContent = h.description;
 
-        // Récupère la class de l'image pour afficher celle correspondante à l'ID du produit
-        // Et ajout du src de l'image et du texte alternatif dans la balise img
-        const img_item = document.querySelector('.item__img');
-        const img_product = document.createElement('img');
-        img_product.src = h.imageUrl;
-        img_product.alt = h.altTxt;
-        img_item.appendChild(img_product);
+    // Récupère la class de l'image pour afficher celle correspondante à l'ID du produit
+    // Et ajout du src de l'image et du texte alternatif dans la balise img
+    const img_item = document.querySelector('.item__img');
+    const img_product = document.createElement('img');
+    img_product.src = h.imageUrl;
+    img_product.alt = h.altTxt;
+    img_item.appendChild(img_product);
 
-        // Ajouter toutes les couleurs disponibles pour ce produit
-        const colors_item = document.querySelector('#colors');
-        for (h.color of h.colors) {
-            let colors_product = document.createElement('option');
-            colors_product.text = h.color;
-            colors_product.value = h.color;
-            colors_item.add(colors_product);
+    // Ajouter toutes les couleurs disponibles pour ce produit
+    const colors_item = document.querySelector('#colors');
+    for (h.color of h.colors) {
+      let colors_product = document.createElement('option');
+      colors_product.text = h.color;
+      colors_product.value = h.color;
+      colors_item.add(colors_product);
+    }
+
+    // Le bouton 'Ajouter au panier'
+    const addToCart = document.querySelector('#addToCart');
+    addToCart.addEventListener('click', (key, value) => {
+
+      // Récupère la quantité choisie par l'utilisateur
+      // En vérifiant si c'est compris entre 1 et 100 et non vide
+      const quantity_item = document.querySelector('#quantity');
+      let TotalProductsCart;
+      if (colors_item.value !== '') {
+        if ((quantity_item.value >= 1) && (quantity_item.value <= 100)) {
+          let quantity = quantity_item.value;
+          let color = colors_item.value;
+
+          // On enregistre le produit et son composant comme le nombre et la couleur
+          let ComposeItemCart = {
+            id: h._id,
+            name: h.name,
+            quantity: quantity,
+            color: color
+          };
+          // On initie le panier dans le localStorage s'il est vide
+          if (localStorage.getItem('TotalProductsCart') === null) {
+            console.info('Le panier n\'existe pas encore. Création du panier.');
+            // On ajoute le premier Item dans le panier
+            let FirstItemOnCart = JSON.stringify(ComposeItemCart);
+            localStorage.setItem('TotalProductsCart', FirstItemOnCart);
+          } else {
+            // On ajoute le produit au panier
+            TotalProductsCart = JSON.parse(localStorage.getItem('TotalProductsCart'));
+            localStorage.setItem('TotalProductsCart', JSON.stringify(TotalProductsCart));
+          }
+
+        } else {
+          alert('S\'il vous plait, entrez une quantité valide entre 1 et 100');
         }
-
-        // Le bouton 'Ajouter au panier'
-        const addToCart = document.querySelector('#addToCart');
-        addToCart.addEventListener('click', () => {
-
-            // Récupère la quantité choisie par l'utilisateur
-            // En vérifiant si c'est compris entre 1 et 100 et non vide
-            const quantity_item = document.querySelector('#quantity');
-            if (colors_item.value !== '') {
-                if ((quantity_item.value >= 1) && (quantity_item.value <= 100)) {
-                    let quantity = quantity_item.value;
-                    let color = colors_item.value;
-                    // On enregistre le produit et son composant comme le nombre et la couleur
-                    const ComposeQuantityItem = [{
-                        name: h.name,
-                        quantity: quantity,
-                        color: color,
-                    }]
-                    const OrderQuantityItem = JSON.stringify(ComposeQuantityItem);
-                    console.table(OrderQuantityItem)
-                    // On l'enregistre dans le localStorage
-                    localStorage.setItem('OrderQuantityItem', OrderQuantityItem);
-                } else {
-                    console.log(quantity_item.value);
-                    alert('S\'il vous plait, entrez une quantité valide entre 1 et 100');
-                }
-            } else {
-                alert('S\'il vous plait, entrez une couleur');
-            }
-
-
-        })
-
+      } else {
+        alert('S\'il vous plait, entrez une couleur');
+      }
     });
+  });
