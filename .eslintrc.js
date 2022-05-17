3ff202253d5654a8219a15d20142c351226358ba
Thomas Bnt <thomasbnt@protonmail.com>
2022-05-17 15:06:09 +0200

    Modif eslint airbnb config + config jetbrains

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+  plugins: [
+    'prettier',
+  ],
+};
