273410d4fbfd20bd1fac707aafffc3c1859084b0
Thomas Bnt <thomasbnt@protonmail.com>
2022-05-17 15:06:24 +0200

    Cleanup eslint

--- front/js/script.js
+++ front/js/script.js
@@ -1,13 +1,14 @@
-const base_url = 'http://localhost:3000/api/';
-const product_url = base_url + 'products';
+const baseUrl = 'http://localhost:3000/api/';
+const productUrl = `${baseUrl}products`;
 
-const product_list = document.querySelector('#items');
+const productList = document.querySelector('#items');
 
-fetch(product_url)
+fetch(productUrl)
   .then((response) => response.json())
   .then((data) => {
-    for (let d of data) {
-      let product = document.createElement('a');
+    for (const d of data) {
+      const product = document.createElement('a');
+      // eslint-disable-next-line no-underscore-dangle
       product.href = `./product.html?id=${d._id}`;
       product.innerHTML = `
             <article>
@@ -16,12 +17,12 @@ fetch(product_url)
               <p class='productDescription'>${d.description}</p>
             </article>
             `;
-      product_list.appendChild(product);
+      productList.appendChild(product);
     }
-  }).catch((err) => {
-// Sélectionne le contenu de l'item et affiche une erreur s'il n'y a pas de produit ou que l'API est indisponible
-  const ErrMessageInContent = document.querySelector('.titles');
-  console.log(ErrMessageInContent);
-  ErrMessageInContent.innerHTML = `<h1>Erreur 503</h1><h2>Impossible de récupérer les articles depuis l\'API.</h2>`;
-});
-
+  }).catch(() => {
+    // Sélectionne le contenu de l'item et affiche une erreur
+    // s'il n'y a pas de produit ou que l'API est indisponible
+    const errMessageInContent = document.querySelector('.titles');
+    console.log(errMessageInContent);
+    errMessageInContent.innerHTML = '<h1>Erreur 503</h1><h2>Impossible de récupérer les articles depuis l\'API.</h2>';
+  });
